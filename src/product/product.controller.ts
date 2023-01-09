88d972ea148730554d48749ac09b6346f538b653
Felipe Romao <feliperomao.a@gmail.com>
2023-01-09 18:27:41 -0300

    docs: :memo: improving doc for endpoints in swagger

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { ProductService } from './product.service';
 
 @ApiTags('Produtcs')
@@ -7,16 +7,19 @@ import { ProductService } from './product.service';
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
+  @ApiOperation({ summary: 'Find products sorted alphabetically' })
   @Get('findByAlphabeticalOrder')
   findByAlphabeticalOrder() {
     return this.productService.findByAlphabeticalOrder();
   }
 
+  @ApiOperation({ summary: 'Find expired products today' })
   @Get('findByExpiredToday')
   findByExpiredToday() {
     return this.productService.findByProductsExpiredToday();
   }
 
+  @ApiOperation({ summary: 'Find products that will expire tomorrow' })
   @Get('findExpireTomorrow')
   findExpireTomorrow() {
     return this.productService.findExpireTomorrow();
